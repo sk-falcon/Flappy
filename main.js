cceec00b61128363fa98b44ce7a723e899a7ab1c
sk-falcon <sparsh.dtu@gmail.com>
2016-12-22 12:56:00 +0530

    Main Structure Implemented

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var mainState = {
   preload: function() {
     //Load bird sprite
     game.load.image('bird', 'assets/bird.png');
+    game.load.image('pipe', 'assets/pipe.png');
 
   },
 
@@ -14,11 +15,19 @@ var mainState = {
 
     game.state.backgroundColor = '#71c5cf';
 
+    //Initiate Game Score
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0",
+          { font: "30px Arial", fill: "#ffffff" });
+
     // Set the game Physic System
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     // Display the bird
     this.bird = game.add.sprite(100, 245, 'bird');
+
+    //Create an empty group
+    this.pipes = game.add.group();
 
     // Add physics to bird
     game.physics.arcade.enable(this.bird);
@@ -31,6 +40,9 @@ var mainState = {
                     Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
 
+    //run timer to add pipes
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
   },
 
   // Game Logic
@@ -39,6 +51,9 @@ var mainState = {
     //Out of screen bird, call the restart function
     if (this.bird.y < 0 || this.bird.y > 490)
       this.restartGame();
+
+    game.physics.arcade.overlap(
+      this.bird, this.pipes, this.restartGame, null, this);
 
   },
 
@@ -53,6 +68,43 @@ var mainState = {
     //Start the main state
     game.state.start('main');
   },
+
+  //Create a pipe at location x, y
+  addOnePipe: function(x, y) {
+
+    //Create a pipe sprite
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    //Add the pipe to group
+    this.pipes.add(pipe);
+
+    //enable physics on the pipe
+    game.physics.arcade.enable(pipe);
+
+    //add velocity
+    pipe.body.velocity.x = -200;
+
+    //kill pipe when not in screen
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+
+  },
+
+  addRowOfPipes: function() {
+
+    //Pick a number between 1 and 5
+    var hole = Math.floor(Math.random()*5) + 1;
+
+    //Add the 5 pipes
+    for(var i=0; i<8; i++)
+      if(i!=hole && i!=hole+1 && i!=hole+2)
+        this.addOnePipe(400, i * 60 +10);
+
+    //Update Score
+    this.score += 1;
+    this.labelScore.text = this.score;
+
+    },
 
 };
 
